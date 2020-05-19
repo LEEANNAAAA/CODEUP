1809545bec06c7db5e69632751a12f4a6fa50773
Anna Lee <ssmapricity93@gmail.com>
2020-05-19 19:52:07 +0900

    단어 1개 입력받아 그대로 출력

---
+++ c_1021.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+​
+
+int main()
+
+{
+
+char a[51]="";
+
+scanf("%s",&a);
+
+printf("%s",a);
+
+​
+
+return 0;
+
+}
+
+​
+
+51은 50문자까지 인식 가능.
+
+char a[51]; 까지만 해도 인식 가능.
+
+​
